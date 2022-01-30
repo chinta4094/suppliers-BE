797c296508327d8aedcae94245b4514df358c2f1
sribhaskar <sribhaskar4094@gmail.com>
2022-01-30 21:57:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const dataBase = require('./db/db')
+const adminRouter = require('./routes/adminRouter')
+const itemRouter = require('./routes/itemRouter')
+const userRouter = require('./routes/userRouter')
+
+const app = express()
+const port = process.env.PORT || 8000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended : true }))
+
+dataBase()
+
+app.use('/admin',adminRouter)
+app.use('/item',itemRouter)
+app.use('/user',userRouter)
+
+app.get('/',(req,res) => {
+    res.send("HELLO HEROKU ....")
+})
+
+app.listen(port, () => console.log(`Server is running on port : ${port}`))
