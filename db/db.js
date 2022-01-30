bb489391338739ef382714c1a5d64854202c1041
sribhaskar <sribhaskar4094@gmail.com>
2022-01-30 23:35:37 +0530

    first commit

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const { Model } = require('objection')
 const knexfile = require('../knexfile')
 
-const product = process.env.DB_ENVIRONMENT || 'development'
+const product = 'production' || 'development'
 const connectDB = () =>{
     const db = knex(knexfile[product])
     Model.knex(db)
