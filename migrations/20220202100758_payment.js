772e574793aa0d00321556a66a7ff3aa7c35f178
sribhaskar <sribhaskar4094@gmail.com>
2022-02-02 15:46:36 +0530

    first commit

---
+++ migrations/20220202100758_payment.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+  .createTable('payment',(table) => {
+      table.increments('id');
+      table.string('email');
+      table.integer('total')
+      table.timestamps(true,true)
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('payment')
+};
